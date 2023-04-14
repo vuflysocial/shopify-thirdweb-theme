b2c4f9d3f0930aa5145d298683609177f6d98455
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-14 11:54:05 -0700

    Delete melotokengate.js

--- scripts/melotokengate.js
+++
@@ -1,9 +0,0 @@
-const elements = document.querySelectorAll(".melotokengate.js");
-
-// We loop through all the elements that have the class name "your-component" and render our React component inside of it, this allows you to add multiple instances of the same component to different pages.
-
-elements &&
-  [...elements].forEach((node) => {
-    const root = createRoot(node);
-    root.render(<YourComponent />);
-  });
